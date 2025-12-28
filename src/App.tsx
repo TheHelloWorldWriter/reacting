7c3fc987e7a397a76b0745ed97cc667a9608f958
TechAurelian <dev@techaurelian.com>
2025-12-28 19:07:01 +0200

    Refactor BackgroundDecoration component instances for improved layout and responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -7,17 +7,23 @@ function App() {
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-[#087ea4] via-[#61DAFB] to-[#087ea4] relative overflow-hidden">
-      {/* Background Decorations */}
-      <BackgroundDecoration top="8%" left="12%" text={greeting} />
-      <BackgroundDecoration top="15%" left="65%" text={greeting} />
-      <BackgroundDecoration top="35%" left="5%" text={greeting} />
-      <BackgroundDecoration top="60%" left="75%" text={greeting} />
-      <BackgroundDecoration top="75%" left="20%" text={greeting} />
-      <BackgroundDecoration top="25%" left="80%" text={greeting} />
-      <BackgroundDecoration top="85%" left="60%" text={greeting} />
-      <BackgroundDecoration top="45%" left="85%" text={greeting} />
-      <BackgroundDecoration top="10%" left="35%" text={greeting} className="hidden lg:block" />
-      <BackgroundDecoration top="70%" left="40%" text={greeting} className="hidden xl:block" />
+      {/* Background Decorations - Always visible (mobile-first) */}
+      <BackgroundDecoration top="10%" left="5%" text={greeting} />
+      <BackgroundDecoration top="20%" left="75%" text={greeting} />
+      <BackgroundDecoration top="70%" left="15%" text={greeting} />
+      <BackgroundDecoration top="80%" left="70%" text={greeting} />
+
+      {/* Additional decorations for medium screens and up */}
+      <BackgroundDecoration top="15%" left="40%" text={greeting} className="hidden md:block" />
+      <BackgroundDecoration top="50%" left="80%" text={greeting} className="hidden md:block" />
+
+      {/* Additional decorations for large screens and up */}
+      <BackgroundDecoration top="35%" left="10%" text={greeting} className="hidden lg:block" />
+      <BackgroundDecoration top="60%" left="50%" text={greeting} className="hidden lg:block" />
+
+      {/* Additional decorations for extra-large screens only */}
+      <BackgroundDecoration top="45%" left="25%" text={greeting} className="hidden xl:block" />
+      <BackgroundDecoration top="85%" left="45%" text={greeting} className="hidden xl:block" />
 
       {/* Main Dialog */}
       <GreetingDialog
